531cddd9af05248a41974ca444a58dc2002c6990
pg22nicholas <pg22nicholas@vfs.com>
2022-04-12 13:21:11 -0700

    Added rotation to playing character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
 
     Rigidbody2D RigidBody;
+    private float m_MaxZRot = 38f;
+    private float m_MinZRot = -90f;
+
     void Start()
     {
         RigidBody = GetComponent<Rigidbody2D>();
@@ -17,6 +20,22 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             RigidBody.velocity = Vector2.up * 4.5f;
+        }
+
+        float currEulerAngle = transform.eulerAngles.z;
+        // set from -180 to 180
+        currEulerAngle = (currEulerAngle > 180) ? currEulerAngle - 360 : currEulerAngle;
+        // If going upwards, apply upwards z rotation
+        if (RigidBody.velocity.y > 0)
+        {
+            currEulerAngle += 500 * Time.deltaTime;
+            transform.localRotation = Quaternion.Euler(0, 0, Mathf.Clamp(currEulerAngle, m_MinZRot, m_MaxZRot));
+        } 
+        // applt downwards z rotation
+        else
+        {
+            currEulerAngle -= 200 * Time.deltaTime;
+            transform.localRotation = Quaternion.Euler(0, 0, Mathf.Clamp(currEulerAngle, m_MinZRot, m_MaxZRot));
         }
     }
 
